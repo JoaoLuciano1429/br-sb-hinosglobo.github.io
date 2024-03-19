2bb222a5e59985a39923870c8ba28963b6044dd1
João Luciano <104032132+JoaoLuciano1429@users.noreply.github.com>
2024-03-19 08:24:51 -0300

    Toques finais

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # br-sb-hinosglobo.github.io
+
+# Mudanças:
+
+19/03/2024: Lançamento oficial da versão final do website;
---
+++ js/script.js
@@ -0,0 +1,99 @@
+document.getElementById("AMA").addEventListener("click", (ev) => {
+  ev = document.getElementById("Amazonas");
+  ev.play();
+});
+
+document.getElementById("AMG").addEventListener("click", (ev) => {
+  ev = document.getElementById("AmericaMG");
+  ev.play();
+});
+
+document.getElementById("AVA").addEventListener("click", (ev) => {
+  ev = document.getElementById("Avai");
+  ev.play();
+});
+
+document.getElementById("BFC").addEventListener("click", (ev) => {
+  ev = document.getElementById("BotafogoSP");
+  ev.play();
+});
+
+document.getElementById("BRU").addEventListener("click", (ev) => {
+  ev = document.getElementById("Brusque");
+  ev.play();
+});
+
+document.getElementById("CEA").addEventListener("click", (ev) => {
+  ev = document.getElementById("Ceara");
+  ev.play();
+});
+
+document.getElementById("CHA").addEventListener("click", (ev) => {
+  ev = document.getElementById("Chapecoense");
+  ev.play();
+});
+
+document.getElementById("CFC").addEventListener("click", (ev) => {
+  ev = document.getElementById("Coritiba");
+  ev.play();
+});
+
+document.getElementById("CRB").addEventListener("click", (ev) => {
+  ev = document.getElementById("CR_Brasil");
+  ev.play();
+});
+
+document.getElementById("GOI").addEventListener("click", (ev) => {
+  ev = document.getElementById("Goias");
+  ev.play();
+});
+
+document.getElementById("NOV").addEventListener("click", (ev) => {
+  ev = document.getElementById("Novorizontino");
+  ev.play();
+});
+
+document.getElementById("GFC").addEventListener("click", (ev) => {
+  ev = document.getElementById("Guarani");
+  ev.play();
+});
+
+document.getElementById("ITU").addEventListener("click", (ev) => {
+  ev = document.getElementById("Ituano");
+  ev.play();
+});
+
+document.getElementById("MIR").addEventListener("click", (ev) => {
+  ev = document.getElementById("Mirassol");
+  ev.play();
+});
+
+document.getElementById("OPF").addEventListener("click", (ev) => {
+  ev = document.getElementById("OperarioFerroviario");
+  ev.play();
+});
+
+document.getElementById("PAY").addEventListener("click", (ev) => {
+  ev = document.getElementById("Paysandu");
+  ev.play();
+});
+
+document.getElementById("PON").addEventListener("click", (ev) => {
+  ev = document.getElementById("PontePreta");
+  ev.play();
+});
+
+document.getElementById("SAN").addEventListener("click", (ev) => {
+  ev = document.getElementById("Santos");
+  ev.play();
+});
+
+document.getElementById("SPO").addEventListener("click", (ev) => {
+  ev = document.getElementById("SportRecife");
+  ev.play();
+});
+
+document.getElementById("VIL").addEventListener("click", (ev) => {
+  ev = document.getElementById("VilaNova");
+  ev.play();
+});
